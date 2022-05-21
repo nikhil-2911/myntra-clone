50131f8dfefeca7956d78825c99f8e92e52a4bdf
nikhil-2911 <nikhilxmr5@gmail.com>
2022-05-21 11:48:13 +0530

    showing products to ui and product card design complete

--- src/components/Products/ProductCard/ProductCard.jsx
+++ src/components/Products/ProductCard/ProductCard.jsx
@@ -1,8 +1,22 @@
 import React from "react";
+import "./ProductCard.css";
 
 const ProductCard = ({ product }) => {
   console.log(product);
-  return <div>ProductCard</div>;
+  return (
+    <div id="productCard">
+      <img className="profilePhoto" src={product.images[0].src} alt="#photo" />
+      <div id="productDetails">
+        <p className="brandText">{product.brand}</p>
+        <p className="infoText">{product.additionalInfo}</p>
+        <span className="productPrice">{`Rs. ${product.price}`}</span>
+        <span className="productMrp">
+          <s>{`Rs. ${product.mrp}`}</s>
+        </span>
+        <span className="productDiscount">{`${product.discountDisplayLabel}`}</span>
+      </div>
+    </div>
+  );
 };
 
 export default ProductCard;
